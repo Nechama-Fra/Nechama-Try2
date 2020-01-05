ad408983e0c4dd6a1942dec40a2a408b71ebfbd3
NFraind@madisoncres.com <Nechama-Fra>
2020-01-05 13:38:49 +0200

    commit1

--- cypress/integration/TestWithJsonAndFunctionWithArray.js
+++ cypress/integration/TestWithJsonAndFunctionWithArray.js
@@ -1,3 +1,4 @@
+///<reference types="Cypress" />
 describe('My First Test', function() {
    
     before(function() {
@@ -10,7 +11,7 @@ describe('My First Test', function() {
    
    
     it('AddToCard', function() {
-    cy.visit('https://rahulshettyacademy.com/angularpractice/shop')
+    cy.visit('https://rahulshettyacademy.com/angularpractice/shop1')
     cy.log(this.data.productName[0])
     this.data.productName.forEach(function(element){
         cy.selectProduct(element)
